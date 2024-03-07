c894600557961c9b56cc03e82b29f12c3dbbd23c
machinawindowscyber <machina@windows.cyber>
2024-03-07 13:23:04 +0300

    filtering records

--- resources/views/users/add.blade.php
+++ resources/views/users/add.blade.php
@@ -98,7 +98,6 @@
                         <select
                             class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
                             data-trigger name="status" id="status-field">
-                            <option value="">Status</option>
                             <option value="Active">Active</option>
                             <option value="Block">Block</option>
                         </select>
