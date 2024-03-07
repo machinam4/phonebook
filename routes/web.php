9db360e9a5c4b9a9016dc066af75d48cea9de753
machinawindowscyber <machina@windows.cyber>
2024-03-07 16:28:28 +0300

    home coming

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('coming_soon');
 });
 
 Route::middleware(['auth'])->group(function () {
