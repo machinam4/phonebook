dae1acbe2d425df339eda950206558444203a34c
machinawindowscyber <machina@windows.cyber>
2024-03-18 13:43:22 +0300

    test paybill

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -48,7 +48,7 @@ class ContactsController extends Controller
     {
         // Extract DebitPartyName from the callback data
         // Log::info("requets back");
-        // Log::error($request);
+        Log::error($request);
 
         if ($request->json('Result.ResultCode') != 0) {
             Log::info($request);
